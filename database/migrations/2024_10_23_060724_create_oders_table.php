424ff17eb03089986d6a1fcbc5b313e20d288d52
Ngaily <nguyenaily1207@gmail.com>
2024-10-23 16:21:13 +0700

    sources code mới

---
+++ database/migrations/2024_10_23_060724_create_oders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('oders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('users_id');
+            $table->decimal('total', 10, 2);
+            $table->integer('status');
+            $table->integer('payment_method');
+            $table->text('address');
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('oders');
+    }
+};
